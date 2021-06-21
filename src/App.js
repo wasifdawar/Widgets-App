c2ba69e1015ef32ccbbec2345fa5e41d7e4a0418
wasifdawar <m.wasifdawar@gmail.com>
2021-06-21 23:08:54 +0500

    Stephen Grinder Tutorial App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+];
 
-export default App;
+export default () => {
+    const items = [
+        {
+            title: 'What is React?',
+            content: 'React is a JS libaray'
+        },
+        {
+            title: 'What is React?',
+            content: 'React is a JS libaray'
+        },
+        {
+            title: 'What is React?',
+            content: 'React is a JS libaray'
+        }
+    ];
+    
+    return (
+        <div>
+        
+            <Search/>
+            <Dropdown options={options}/>
+        </div>
+    )
+};
